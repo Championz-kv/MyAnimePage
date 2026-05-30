62aa96e9ee62b76e12980d3c004cd6a95e4daa77
Championz <87133884+Championz-kv@users.noreply.github.com>
2026-05-30 15:45:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,235 @@
+const animesascending = [
+    { name: "365 Days to the Wedding", image: "images/365-days-to-the-wedding.jpg" },
+    { name: "86", image: "images/86.jpg" },
+
+    { name: "Agents of the Four Seasons: Dance of Spring", image: "images/agents-of-the-four-seasons.jpg" },
+    { name: "Akudama Drive", image: "images/akudama-drive.jpg" },
+    { name: "Alya Sometimes Hides Her Feelings in Russian", image: "images/alya-sometimes-hides-her-feelings-in-russian.jpg" },
+    { name: "Ameku M.D.: Doctor Detective", image: "images/ameku-md-doctor-detective.jpg" },
+    { name: "The Angel Next Door Spoils Me Rotten", image: "images/the-angel-next-door-spoils-me-rotten.jpg" },
+    { name: "Another", image: "images/another.png" },
+    { name: "The Apothecary Diaries", image: "images/the-apothecary-diaries.jpg" },
+    { name: "Assassination Classroom", image: "images/assassination-classroom.jpg" },
+
+    { name: "The Beginning After the End", image: "images/the-beginning-after-the-end.jpg" },
+    { name: "Blue Box", image: "images/blue-box.jpg" },
+    { name: "Bocchi the Rock!", image: "images/bocchi-the-rock.jpg" },
+    { name: "Bubble", image: "images/bubble.jpg" },
+    { name: "Buddy Daddies", image: "images/buddy-daddies.jpg" },
+    { name: "Bungo Stray Dogs", image: "images/bungo-stray-dogs.jpg" },
+
+    { name: "The Case Study of Vanitas", image: "images/the-case-study-of-vanitas.jpg" },
+    { name: "Chainsaw Man", image: "images/chainsaw-man.png" },
+    { name: "Classroom of the Elite", image: "images/classroom-of-the-elite.jpg" },
+    { name: "Colorful Stage! The Movie: A Miku Who Can't Sing", image: "images/colorful-stage-movie.jpg" },
+    { name: "Cosmic Princess Kaguya!", image: "images/cosmic-princess-kaguya.jpg" },
+
+    { name: "Dandadan", image: "images/dandadan.jpg" },
+    { name: "Darling in the FRANXX", image: "images/darling-in-the-franxx.png" },
+    { name: "Death Note", image: "images/death-note.jpg" },
+    { name: "Demon Lord 2099", image: "images/demon-lord-2099.jpg" },
+    { name: "Demon Slayer", image: "images/demon-slayer.jpg" },
+    { name: "Demon Sword Master of Excalibur Academy", image: "images/demon-sword-master.jpg" },
+    { name: "The Detective Is Already Dead", image: "images/the-detective-is-already-dead.jpg" },
+
+    { name: "Erased", image: "images/erased.jpg" },
+
+    { name: "The Fragrant Flower Blooms with Dignity", image: "images/the-fragrant-flower-blooms-with-dignity.jpg" },
+    { name: "Frieren: Beyond Journey's End", image: "images/frieren.jpg" },
+    { name: "Fruits Basket", image: "images/fruits-basket.jpg" },
+
+    { name: "The Garden of Words", image: "images/the-garden-of-words.jpg" },
+    { name: "Grand Blue Dreaming", image: "images/grand-blue-dreaming.png" },
+
+    { name: "Hana Kimi", image: "images/hana-kimi.jpg" },
+    { name: "Headhunted to Another World: From Salaryman to Big Four!", image: "images/headhunted-to-another-world.jpg" },
+    { name: "Hell's Paradise", image: "images/hells-paradise.jpg" },
+    { name: "Hello World", image: "images/hello-world.png" },
+    { name: "Honey Lemon Soda", image: "images/honey-lemon-soda.jpg" },
+    { name: "Horimiya", image: "images/horimiya.jpg" },
+
+    { name: "I Made Friends with the Second Prettiest Girl in My Class", image: "images/second-prettiest-girl.jpg" },
+    { name: "I Want to Eat Your Pancreas", image: "images/i-want-to-eat-your-pancreas.jpg" },
+    { name: "If My Wife Becomes an Elementary School Student", image: "images/if-my-wife-becomes-an-elementary-school-student.jpg" },
+
+    { name: "Jingai-san no Yome", image: "images/jingai-san-no-yome.jpg" },
+    { name: "Josee, the Tiger and the Fish", image: "images/josee-the-tiger-and-the-fish.jpg" },
+    { name: "Jujutsu Kaisen", image: "images/jujutsu-kaisen.jpg" },
+
+    { name: "Kaguya-sama: Love Is War", image: "images/kaguya-sama-love-is-war.png" },
+    { name: "Kaiju No. 8", image: "images/kaiju-no-8.jpg" },
+    { name: "King's Game", image: "images/kings-game.jpg" },
+
+    { name: "Let This Grieving Soul Retire", image: "images/let-this-grieving-soul-retire.png" },
+    { name: "Liar Game", image: "images/liar-game.png" },
+
+    { name: "Maid Sama!", image: "images/maid-sama.png" },
+    { name: "Marriagetoxin", image: "images/marriagetoxin.jpg" },
+    { name: "Mecha-Ude: Mechanical Arms", image: "images/mecha-ude.jpg" },
+    { name: "Monthly Girls' Nozaki-kun", image: "images/monthly-girls-nozaki-kun.png" },
+    { name: "More Than a Married Couple, but Not Lovers", image: "images/more-than-a-married-couple.jpg" },
+    { name: "The Most Notorious Talker Runs the World's Greatest Clan", image: "images/the-most-notorious-talker.jpg" },
+    { name: "My Dress-Up Darling", image: "images/my-dress-up-darling.jpg" },
+    
+    { name: "Oshi no Ko", image: "images/oshi-no-ko.jpg" },
+
+    { name: "The Pet Girl of Sakurasou", image: "images/the-pet-girl-of-sakurasou.jpg" },
+    { name: "Plastic Neesan", image: "images/plastic-neesan.png" },
+    { name: "The Promised Neverland", image: "images/the-promised-neverland.jpg" },
+
+    { name: "The Quintessential Quintuplets", image: "images/the-quintessential-quintuplets.png" },
+
+    { name: "Rascal Does Not Dream Series", image: "images/rascal-does-not-dream.jpg" },
+    { name: "Rent-a-Girlfriend", image: "images/rent-a-girlfriend.jpg" },
+    { name: "Ron Kamonohashi's Forbidden Deductions", image: "images/ron-kamonohashi.jpg" },
+    { name: "Rurouni Kenshin", image: "images/rurouni-kenshin.png" },
+
+    { name: "Sakamoto Days", image: "images/sakamoto-days.png" },
+    { name: "Shangri-La Frontier", image: "images/shangri-la-frontier.jpg" },
+    { name: "Shikimori's Not Just a Cutie", image: "images/shikimori.jpg" },
+    { name: "The Shiunji Family Children", image: "images/the-shiunji-family-children.jpg" },
+    { name: "Solo Leveling", image: "images/solo-leveling.png" },
+    { name: "Spy x Family", image: "images/spyxfamily.png" },
+    { name: "The Strongest Magician in the Demon Lord's Army Was a Human", image: "images/the-strongest-magician.jpg" },
+    { name: "The Summer Hikaru Died", image: "images/the-summer-hikaru-died.jpg" },
+    { name: "Summertime Rendering", image: "images/summertime-rendering.png" },
+    { name: "Suzume", image: "images/suzume.jpg" },
+
+    { name: "Takopi's Original Sin", image: "images/takopis-original-sin.jpg" },
+    { name: "Tasokare Hotel", image: "images/tasokare-hotel.jpg" },
+    { name: "That Time I Got Reincarnated as a Slime", image: "images/tensura.jpg" },
+    { name: "To Every You I've Loved Before", image: "images/to-every-you-ive-loved-before.jpg" },
+    { name: "To Me, the One Who Loved You", image: "images/to-me-the-one-who-loved-you.jpg" },
+    { name: "Toilet-Bound Hanako-kun", image: "images/toilet-bound-hanako-kun.jpg" },
+    { name: "Toradora!", image: "images/toradora.jpg" },
+    { name: "Tower of God", image: "images/tower-of-god.jpg" },
+    { name: "Trapezium", image: "images/trapezium.jpg" },
+    { name: "The Tunnel to Summer, the Exit of Goodbye", image: "images/the-tunnel-to-summer.jpg" },
+
+    { name: "Weathering with You", image: "images/weathering-with-you.png" },
+    { name: "Wind Breaker", image: "images/wind-breaker.jpg" },
+    { name: "Wistoria: Wand and Sword", image: "images/wistoria.jpg" },
+    { name: "Witch Hat Atelier", image: "images/witch-hat-atelier.jpg" },
+
+    { name: "Yakuza Fiance", image: "images/yakuza-fiance.jpg" },
+    { name: "You and I Are Polar Opposites", image: "images/you-and-i-are-polar-opposites.jpg" },
+    { name: "You Are Ms. Servant", image: "images/you-are-ms-servant.jpg" },
+    { name: "Your Lie in April", image: "images/your-lie-in-april.png" },
+    { name: "Your Name", image: "images/your-name.png" },
+
+    { name: "Zom 100: Bucket List of the Dead", image: "images/zom-100.png" } 
+];
+
+// setting anime list to display
+let ascending = true;
+let animeList = animesascending
+
+const numberOfColumns = 5;
+const grid = document.getElementById("anime-grid");
+function renderAnimeGrid() {
+    grid.innerHTML = "";
+    // create columns
+    const columns = [];
+
+    for (let i = 0; i < numberOfColumns; i++) {
+
+        const column = document.createElement("div");
+        column.classList.add("column");
+
+        if (i % 2 === 1) {
+            column.classList.add("offset");
+        }
+        grid.appendChild(column);
+        columns.push(column);
+    }
+
+    const completeRows = Math.floor(animeList.length / numberOfColumns);
+    const normalCount = completeRows * numberOfColumns;
+
+    // Fill all complete rows
+    for (let index = 0; index < normalCount; index++) {
+
+        const anime = animeList[index];
+        const card = document.createElement("div");
+        card.classList.add("anime-card");
+
+        card.innerHTML = `
+            <img src="${anime.image}">
+            <p>${anime.name}</p>
+        `;
+
+        const columnIndex = index % numberOfColumns;
+        columns[columnIndex].appendChild(card);
+    }
+
+    // Create last incomplete row
+    const remaining = animeList.length - normalCount;
+
+    const lastRowPositions = {
+        1: [2],
+        2: [0, 4],
+        3: [0, 2, 4],
+        4: [0, 1, 3, 4]
+    };
+
+    if (remaining > 0) {
+        const positions = lastRowPositions[remaining];
+
+        for (let i = 0; i < remaining; i++) {
+
+            const anime = animeList[normalCount + i];
+            const card = document.createElement("div");
+            card.classList.add("anime-card");
+
+            card.innerHTML = `
+                <img src="${anime.image}">
+                <p>${anime.name}</p>
+            `;
+
+            columns[positions[i]].appendChild(card);
+        }
+    }
+
+    document.getElementById("anime-count").textContent =
+        `${animeList.length} animes included.`;
+}
+
+// create the grid and display anime
+renderAnimeGrid();
+// background lines
+const colors = [
+    "rgba(120, 180, 255, 0.20)", // blue
+    "rgba(120, 255, 180, 0.20)", // green
+    "rgba(255, 235, 120, 0.20)", // yellow
+    "rgba(255, 150, 150, 0.20)", // red
+];
+
+for (let i = 0; i < 30; i++) {
+    const line = document.createElement("div");
+    line.classList.add("bg-line");
+    document.querySelector(".background-lines").appendChild(line);
+
+    line.style.width = (100 + Math.random() * 300) + "px";
+    line.style.left = "-200px";
+    line.style.top = 30 + Math.random() * 250 + "%";
+    line.style.animationDelay = -(Math.random() * 20) + "s";
+    line.style.animationDuration = (10 + Math.random() * 20) + "s";
+    line.style.setProperty("--travel",(2000 + Math.random() * 1000) + "px");
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    line.style.setProperty("--line-color", color);
+}
+
+const sortBtn = document.getElementById("sort-btn");
+
+sortBtn.addEventListener("click", () => {
+
+    ascending = !ascending;
+    animeList = ascending ? animesascending : animesascending.toReversed();
+
+    sortBtn.textContent =
+        ascending
+        ? "▼ Alphabetical"
+        : "▲ Alphabetical";
+
+    renderAnimeGrid();
+});
